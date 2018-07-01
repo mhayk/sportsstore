ddef6866e3ab868c5c07817eb6195027791541b8
Mhayk Whandson <hi@mhayk.com>
2018-07-01 15:42:50 -0400

    The RESTful Web Service has been prepared.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ data.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    return {
+        products: [
+            { id: 1, name: "Kayak", category: "Watersports", description: "A boat for one person", price: 275},
+            { id: 2, name: "Lifejacket", category: "Watersports", description: "Protective and fashionable", price: 48.95}
+        ],
+        orders: []
+    }
+}
